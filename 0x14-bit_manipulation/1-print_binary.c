793fb92b103beb3f928167d5cf8f2d3aa5213b73
salmaQueen <salmawasee9@gmail.com>
2023-04-10 02:48:08 +0300

    second question

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_binary - Function that prints the binary rep of a number
+ * @n: Number of print in a binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int flag = 0;
+
+	while (mask)
+	{
+		if (n & mask)
+		{
+			flag = 1;
+			printf("1");
+		}
+		else if (flag)
+		{
+			printf("0");
+		}
+		mask = mask >> 1;
+	}
+	if (!flag)
+	{
+		printf("0");
+	}
+}
