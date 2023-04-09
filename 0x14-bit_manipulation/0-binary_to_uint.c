4a8d769eced0db61cfa73f5fe7c32fd692237d5e
salmaQueen <salmawasee9@gmail.com>
2023-04-10 02:01:16 +0300

    still on question one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int decimal = 0;
+    unsigned int output = 0;
+    int len = strlen(b);
+    int i=0;
 
-  if (b == NULL)
-  {
-    return 0;
-  }
-  while (*b != '\0')
-  {
-    if (*b != '0' && *b != '1')
+    if(b==NULL)
     {
-      return 0;
+	    return 0;
     }
-    decimal = (decimal << 1) + (*b - '0');
 
-    b++;
-  }
-  return decimal;
+    for (; i < len; i++) {
+        if (b[i] == '1') {
+            output += pow(2, len - i - 1);
+        } else if (b[i] != '0') {
+            return 0;
+        }
+    }
+    return output;
 }
+
