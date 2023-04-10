f855662e8a55470a6791de6531d0e59528049db7
salmaQueen <salmawasee9@gmail.com>
2023-04-10 05:29:16 +0300

    updated again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+
 
 /**
  *binary_to_uint - Entry point convert binary number to
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == NULL)
+	if (!b)
 	{
 		return (0);
 	}
