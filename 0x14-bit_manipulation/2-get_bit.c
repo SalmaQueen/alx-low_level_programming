081cc588b2414149481a128c413b71f9d0c8e21a
salmaQueen <salmawasee9@gmail.com>
2023-04-10 03:20:43 +0300

    get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,9 +7,13 @@
  *@index: The index that the mask will target
  */
 
-int get_bit(unsigned long int n, unsigned int index)
-{
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
-	return ((n >> index) & 1);
+int get_bit(unsigned long int n, unsigned int index) {
+
+  unsigned long int mask = 1UL << index;
+  if (index >= sizeof(unsigned long int) * 8)
+  {
+    return -1; 
+  }
+  
+  return (n & mask) ? 1 : 0;
 }
