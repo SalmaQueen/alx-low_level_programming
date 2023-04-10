6fc10b5f7540063cf1ec1f9c70f23a23c95f60d9
salmaQueen <salmawasee9@gmail.com>
2023-04-10 04:33:31 +0300

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+  unsigned long int mask;
+  if (index >= sizeof(unsigned long int) * 8) {
+    return -1; 
+  }
+   mask = 1UL << index;
+  *n |= mask;
+  return 1;
+}
