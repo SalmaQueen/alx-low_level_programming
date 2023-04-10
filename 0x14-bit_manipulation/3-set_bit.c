1a70b72b469f75a2ea80e58385ce1bf09600123f
salmaQueen <salmawasee9@gmail.com>
2023-04-10 04:35:08 +0300

    again pushed set_bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,8 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
   unsigned long int mask;
-  if (index >= sizeof(unsigned long int) * 8) {
+  if (index >= (sizeof(unsigned long int) * 8) -1)
+  {
     return -1; 
   }
    mask = 1UL << index;
