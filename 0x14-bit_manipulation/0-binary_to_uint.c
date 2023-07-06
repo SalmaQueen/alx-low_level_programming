415c3e31387470914b3bc461fcb8e99b73da4f51
salmaQueen <salmawasee9@gmail.com>
2023-07-06 22:56:38 +0300

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,29 @@
  *
  *Return: 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number = 0;
-	int i = 0;
+    unsigned int k = 1;
+    unsigned int i = 0;
+    int c;
+    unsigned int len;
 
-	if (!b)
-	{
-		return (0);
-	}
-	while (b[i])
-	{
-		if (b[i] != '0' && (b[i] != '1'))
-		{
-		return (0);
-		}
-	number = (number << 1) + (b[i] - '0');
-	i++;
-	}
-	return (number);
+    if (b == NULL)
+        return (0);
+
+    len = strlen(b);
+
+    for (c = len - 1; c >= 0; c--)
+    {
+        if (b[c] != '0' && b[c] != '1')
+            return (0);
+        if (b[c] == '1')
+        {
+            i += k;
+        }
+        k *= 2;
+    }
+    return (i);
 }
+
