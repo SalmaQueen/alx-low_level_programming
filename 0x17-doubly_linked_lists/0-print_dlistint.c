106114f6a1635a6de14600f1973421104a2d1807
salmaQueen <salmawasee9@gmail.com>
2023-08-18 16:34:09 +0300

    new changes

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,16 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a dlistint_t linked list.
- * @h: Pointer to the head of the list.
- * Return: The number of nodes in the list.
+ * print_dlistint - print doubly linked list
+ * @h: list
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	for (; h; nodes++, h = h->next)
+		printf("%d\n", h->n);
 
-    return count;
+	return (nodes);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,12 +1,21 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef __LISTS_H
+#define __LISTS_H
 
-#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-/* Doubly linked list node structure */
+#define __silent __attribute__((__unused__))
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
 typedef struct dlistint_s
 {
     int n;
@@ -14,7 +23,14 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif
